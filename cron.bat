e1e749fc1366293fb614dfa8ea3c6e7a1ada49bd
Karthik-beta <karthikpar@protonmail.com>
2023-10-06 17:06:55 +0530

    Update cron.bat

--- cron.bat
+++ cron.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "D:\Magnum\backend"
-cmd /k C:\Users\DYNA-IPC-510\AppData\Local\Programs\Python\Python311\python.exe run_data3.py
+cmd /c C:\Users\DYNA-IPC-510\AppData\Local\Programs\Python\Python311\python.exe run_data3.py
 exit 
