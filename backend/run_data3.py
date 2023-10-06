34adbdf5f2c23ea28e65bbf7a8f51127a5bdcecb
Karthik-beta <karthikpar@protonmail.com>
2023-10-06 15:44:22 +0530

    Update run_data3.py

--- backend/run_data3.py
+++ backend/run_data3.py
@@ -2,7 +2,11 @@
 # import sys
 import os
 import subprocess
+import time
 
 command = "python manage.py data8"
 subprocess.run(command, shell=True)
 # sys.exit()
+
+# Add a delay of 5 seconds (adjust as needed)
+time.sleep(5)
